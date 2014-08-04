712c4478490fd55e21b46d27ad30c83588e7e033
Lisa Vogt <lisavogt.sf@gmail.com>
2014-08-04 15:30:24 -0700

    created author migration, filled in most fields

--- migrations/20140804133654-author_table_create.js
+++ migrations/20140804133654-author_table_create.js
@@ -1,7 +1,21 @@
 module.exports = {
   up: function(migration, DataTypes, done) {
-    // add altering commands here, calling 'done' when finished
-    done()
+    console.log("running author table creation");
+    migration.createTable('authors',
+      {id: {
+        type: DataTypes.INTGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE,
+      displayname: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      }
+    })
+    .complete(done);
   },
   down: function(migration, DataTypes, done) {
     // add reverting commands here, calling 'done' when finished
