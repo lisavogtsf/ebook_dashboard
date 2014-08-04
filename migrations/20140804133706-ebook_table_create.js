68c2997e899eb2b9c2b7353cc9aff25b45354ef8
Lisa Vogt <lisavogt.sf@gmail.com>
2014-08-04 15:50:13 -0700

    adds ebook table definition

--- migrations/20140804133706-ebook_table_create.js
+++ migrations/20140804133706-ebook_table_create.js
@@ -1,10 +1,31 @@
 module.exports = {
   up: function(migration, DataTypes, done) {
-    // add altering commands here, calling 'done' when finished
-    done()
+    console.log("running ebooks table creation")
+    migration.createTable('ebooks', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE,
+      isbn: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+      title: DataTypes.STRING,
+      ebookUrl: DataTypes.STRING,
+      coverImgUrl: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      // what data type should I use? Float? Decimal?
+      price: DataTypes.FLOAT,
+      priceCategory: DataTypes.STRING
+    })
+    .complete(done);
   },
   down: function(migration, DataTypes, done) {
-    // add reverting commands here, calling 'done' when finished
-    done()
+    migration.dropTable('ebooks')
+    .completed(done);
   }
 }
